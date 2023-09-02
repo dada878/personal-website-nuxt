e7c4e633fbf28c08ed39cc67d98077264ae59c6b
Dada878 <xx0932399@gmail.com>
2023-09-02 14:20:31 +0800

    添加 Latex 支援

--- plugins/markdownit.ts
+++ plugins/markdownit.ts
@@ -1,11 +1,12 @@
 import md from "markdown-it";
+import mathjax from "markdown-it-mathjax3";
 
 export default defineNuxtPlugin(() => {
   const renderer = md({
     html: true,
     linkify: true,
     typographer: true
-  });
+  }).use(mathjax);
   return {
     provide: {
       mdRenderer: renderer,
