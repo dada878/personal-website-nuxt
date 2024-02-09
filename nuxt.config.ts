ce176a6f7ae181e70ed09d65879486dfffd3005f
Dada878 <xx0932399@gmail.com>
2024-02-09 14:16:35 +0800

    add google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import blogs from "./content/blogs.json";
 import projects from "./content/projects.json";
-import { installNuxtSiteConfig, updateSiteConfig } from 'nuxt-site-config-kit'
+import { installNuxtSiteConfig, updateSiteConfig } from "nuxt-site-config-kit";
 import { SitemapEntryInput } from "nuxt-simple-sitemap/dist/runtime/types";
 
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      base_url: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://dada878.com',
-      production_url: 'https://dada878.com',
-    }
+      base_url:
+        process.env.NODE_ENV !== "production"
+          ? "http://localhost:3000"
+          : "https://dada878.com",
+      production_url: "https://dada878.com",
+      gtm: {
+        id: 'GTM-KLBPLFSQ',
+        enabled: true,
+        debug: true,
+        devtools: true,
+      }
+    },
   },
   async setup(options) {
-    await installNuxtSiteConfig()
+    await installNuxtSiteConfig();
   },
-  vue: {  
+  vue: {
     compilerOptions: {
-      isCustomElement: (tag) => ['giscus-widget'].includes(tag),
+      isCustomElement: (tag) => ["giscus-widget"].includes(tag),
     },
   },
   devtools: { enabled: true },
@@ -30,29 +39,40 @@ export default defineNuxtConfig({
       "@fortawesome/free-brands-svg-icons",
     ],
   },
-  modules: ["nuxt-simple-sitemap", "nuxt-simple-robots", "nuxt-schema-org", "nuxt-seo-ui"],
+  modules: [
+    "nuxt-simple-sitemap",
+    "nuxt-simple-robots",
+    "nuxt-schema-org",
+    "nuxt-seo-ui",
+    "@zadigetvoltaire/nuxt-gtm",
+  ],
   site: {
     name: "冰川的個人網站",
     url: "https://dada878.com",
-    defaultLocale: 'tw',
+    defaultLocale: "tw",
     trailingSlash: false,
-    titleSeparator: '|',
-    indexable: process.env.NODE_ENV === 'production',
-    description: "歡迎來到冰川的個人網站！你可以在這裡找到我的作品、部落格、以及聯絡方式～",
+    titleSeparator: "|",
+    indexable: process.env.NODE_ENV === "production",
+    description:
+      "歡迎來到冰川的個人網站！你可以在這裡找到我的作品、部落格、以及聯絡方式～",
   },
   sitemap: {
     urls: async () => {
-      return blogs.map((page) => ({
-        loc: `/blogs/${page.id}`,
-        lastmod: Date().toString(),
-        changefreq: "daily",
-        priority: 0.8,
-      })).concat(projects.map((page) => ({
-        loc: `/projects/${page.id}`,
-        lastmod: Date().toString(),
-        changefreq: "daily",
-        priority: 0.8,
-      }))) as SitemapEntryInput[];
+      return blogs
+        .map((page) => ({
+          loc: `/blogs/${page.id}`,
+          lastmod: Date().toString(),
+          changefreq: "daily",
+          priority: 0.8,
+        }))
+        .concat(
+          projects.map((page) => ({
+            loc: `/projects/${page.id}`,
+            lastmod: Date().toString(),
+            changefreq: "daily",
+            priority: 0.8,
+          }))
+        ) as SitemapEntryInput[];
     },
   },
   generate: {
@@ -65,6 +85,6 @@ export default defineNuxtConfig({
   },
   robots: {
     allow: "/",
-    disallow: ['/blogs', '/projects'],
+    disallow: ["/blogs", "/projects"],
   },
 });
