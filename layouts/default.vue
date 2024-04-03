aad9031a78caf7802ab49126ea7baa3f2c026ea0
Dada878 <xx0932399@gmail.com>
2024-04-03 15:53:16 +0800

    migrate to vercel's new speed insights package

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SpeedInsights />
     <PageNavbar />
     <slot />
     <PageFooter />
@@ -9,4 +10,5 @@
 <script setup lang="ts">
 import PageNavbar from "@/components/layout/PageNavbar.vue";
 import PageFooter from "@/components/layout/PageFooter.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 </script>
